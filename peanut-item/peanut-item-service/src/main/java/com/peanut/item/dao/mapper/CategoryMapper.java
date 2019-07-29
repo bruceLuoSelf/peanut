05cfcf2df8fe8409f76b389077e37747c0515c1b
luojiannan <399129811@qq.com>
2019-07-29 19:06:14 +0800

    生成mapper文件

---
+++ peanut-item/peanut-item-service/src/main/java/com/peanut/item/dao/mapper/CategoryMapper.java
@@ -0,0 +1,7 @@
+package com.peanut.item.dao.mapper;
+
+import com.peanut.item.dao.entity.Category;
+import tk.mybatis.mapper.common.Mapper;
+
+public interface CategoryMapper extends Mapper<Category> {
+}
